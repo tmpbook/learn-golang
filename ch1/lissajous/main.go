799fb570078cf1aa2ba025245de7b796469c6ae0
tmpbook <nauy2011@126.com>
2017-02-22 01:46:49 +0800

    增强lissajous程序，将.gif输出到Web

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -10,6 +10,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 var palette = []color.Color{color.White, color.Black}
@@ -23,7 +24,7 @@ func main() {
 	// The sequence of images is deterministic unless we seed
 	// the pseudo-random number generator using the current time.
 	// Thanks to Randall McPherson for pointing out the omission.
-	// rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 
 	if len(os.Args) > 1 && os.Args[1] == "web" {
 		//!+http
