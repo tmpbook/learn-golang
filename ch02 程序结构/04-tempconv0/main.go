abeff126ffd1bf9cc7bb6ccd7af5649c414b1614
tmpbook <nauy2011@126.com>
2017-03-18 21:56:13 +0800

    不同温度单位分别定义为不同的类型

--- ch02 程序结构/04-tempconv0/main.go
+++ ch02 程序结构/04-tempconv0/main.go
@@ -13,3 +13,4 @@ func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
 // Celsius 和 Fahrenheit 虽然都是 float64 但是他们不是同一个数据类型
+// 好累
